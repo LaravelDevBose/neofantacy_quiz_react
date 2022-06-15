f65e8f7699626d2af610c041f584525e2670d756
LaravelDevBose <laravel.devbose@gmail.com>
2022-06-16 03:43:22 +0600

    upodate

--- src/components/QRCode/QRScan.js
+++ src/components/QRCode/QRScan.js
@@ -20,8 +20,8 @@ function QRCodeReader({ handleScan }, ref) {
         style={previewStyle}
         onError={handleError}
         onScan={handleScan}
-        // facingMode="rear"
-        constraints="{ facingMode: 'application' }"
+        facingMode="application"
+        // constraints="{ facingMode: 'application' }"
       />
     </div>
   );
