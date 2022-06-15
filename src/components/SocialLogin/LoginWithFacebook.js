c724d8a6c1ac96a890f63065c2a92ddb57fe206a
abid1174 <abidalamin7@gmail.com>
2022-06-15 21:25:50 +0600

    facebook button fix

--- src/components/SocialLogin/LoginWithFacebook.js
+++ src/components/SocialLogin/LoginWithFacebook.js
@@ -8,28 +8,35 @@ function LoginWithFacebook() {
   };
 
   return (
-    <FacebookLogin
-      appId="1088597931155576"
-      autoLoad={false}
-      fields="name,email,picture"
-      // onClick={componentClicked}
-      callback={responseFacebook}
-      render={(renderProps) => (
-        <button
-          className="btn"
-          onClick={renderProps.onClick}
-          disabled={renderProps.isDisabled}
-          style={{
-            backgroundColor: "#3B5998",
-            border: "2px solid #3B5998",
-            color: "#fff",
-          }}
-        >
-          {" "}
-          <img src={Facebook} alt="google" /> Sign in with Facebook
-        </button>
-      )}
-    />
+    <div
+      className="fb-login"
+      style={{ width: "100%", display: "flex", justifyContent: "center" }}
+    >
+      <FacebookLogin
+        appId="1088597931155576"
+        autoLoad={false}
+        fields="name,email,picture"
+        // onClick={componentClicked}
+        callback={responseFacebook}
+        render={(renderProps) => (
+          <button
+            className="btn"
+            onClick={renderProps.onClick}
+            disabled={renderProps.isDisabled}
+            style={{
+              // backgroundColor: "#3B5998",
+              // border: "2px solid #3B5998",
+              color: "#fff",
+              border: "2px solid rgba(0, 0, 0, 0.15)",
+              marginBottom: "1rem",
+            }}
+          >
+            {" "}
+            <img src={Facebook} alt="facebook" /> Sign in with Facebook
+          </button>
+        )}
+      />
+    </div>
   );
 }
 export default LoginWithFacebook;
