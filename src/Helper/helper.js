77d8a6cb85f57ecb9b7c97e38c6721efb902d3a7
Yash Mathur <65745613+theyashmathur@users.noreply.github.com>
2022-06-15 10:48:12 +0530

    Update helper.js

--- src/Helper/helper.js
+++ src/Helper/helper.js
@@ -54,3 +54,66 @@ export const fetchJsonData = async (url) => {
 
 //   return nftData;
 // };
+
+const Web3 = require('web3');
+const web3 = new Web3(window.ethereum);
+const isMetaMaskInstalled = () => {
+  //Have to check the ethereum binding on the window object to see if it's installed
+  const { ethereum } = window;
+
+  return Boolean(ethereum && ethereum.isMetaMask);
+};
+
+export const initialize = async() => {
+  let accounts
+  const isMetaMaskConnected = () => accounts && accounts.length > 0
+
+  //Created check function to see if the MetaMask extension is installed
+  const isMetaMaskInstalled = () => {
+    //Have to check the ethereum binding on the window object to see if it's installed
+    const { ethereum } = window;
+
+    return Boolean(ethereum && ethereum.isMetaMask);
+  };
+
+  const MetamaskClientCheck = async() => {
+    //Now we check to see if Metmask is installed
+    if (!isMetaMaskInstalled()) {
+      //If it isn't installed we ask the user to click to install it
+      //onboardButton.innerText = 'Click here to install MetaMask!';
+      alert('Please install MetaMask');
+    } else if(!isMetaMaskConnected()) {
+      //If MetaMask is installed we ask the user to connect to their wallet
+      //onboardButton.innerText = 'Connect';
+      onClickConnect();
+    }
+
+  };
+
+  //const onboarding = new MetamaskOnboarding({ forwarderOrigin });
+  MetamaskClientCheck();
+  // nftUri();
+}
+
+
+export const onClickConnect = async () => {
+  try {
+    const { ethereum } = window;
+    if (!isMetaMaskInstalled()){
+      alert('Please Install Metamask');
+    }
+    //Will Start the MetaMask Extension
+    let accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+    if(accounts[0]!= ''){
+      return accounts[0];
+      // $("#connect_wallet").html("Connected");
+      // $("#connect_wallet").prop( "disabled", true );
+    }else{
+      alert('Something Wrong. Metamask not connected');
+      return  false;
+    }
+
+  } catch (error) {
+    console.error(error);
+  }
+};
