3bc73a44aa9459e1e918c0d4a56c621d85be33f2
sohantalukder <sohan15-1933@diu.edu.bd>
2022-06-17 12:21:52 +0600

    add reward

--- src/Helper/helper.js
+++ src/Helper/helper.js
@@ -1,4 +1,4 @@
-export const API_URL = "https://quizadmin.neofantasy.io/api";
+export const API_URL = 'https://quizadmin.neofantasy.io/api';
 
 // export function hideMyModal(modalId) {
 //   $(`${modalId}`).hide();
@@ -14,29 +14,29 @@ export const API_URL = "https://quizadmin.neofantasy.io/api";
 var token;
 
 export function setToken(tokenString) {
-  token = tokenString;
-  localStorage.setItem("token", JSON.stringify(token));
+	token = tokenString;
+	localStorage.setItem('token', JSON.stringify(token));
 }
 
 export function getToken() {
-  if (!token) {
-    token = JSON.parse(localStorage.getItem("token"));
-  }
+	if (!token) {
+		token = JSON.parse(localStorage.getItem('token'));
+	}
 
-  return token;
+	return token;
 }
 
 export function removeToken() {
-  token = "";
-  localStorage.removeItem("token");
+	token = '';
+	localStorage.removeItem('token');
 }
 
 export const fetchJsonData = async (url) => {
-  return fetch(url)
-    .then((response) => response.json())
-    .then((response) => {
-      return response;
-    });
+	return fetch(url)
+		.then((response) => response.json())
+		.then((response) => {
+			return response;
+		});
 };
 
 // var nftData;
@@ -58,54 +58,51 @@ export const fetchJsonData = async (url) => {
 const Web3 = require('web3');
 const web3 = new Web3(window.ethereum);
 const isMetaMaskInstalled = () => {
-  //Have to check the ethereum binding on the window object to see if it's installed
-  const { ethereum } = window;
+	//Have to check the ethereum binding on the window object to see if it's installed
+	const { ethereum } = window;
 
-  return Boolean(ethereum && ethereum.isMetaMask);
+	return Boolean(ethereum && ethereum.isMetaMask);
 };
 
-export const initialize = async() => {
-  let accounts
-  const isMetaMaskConnected = () => accounts && accounts.length > 0
+export const initialize = async () => {
+	let accounts;
+	const isMetaMaskConnected = () => accounts && accounts.length > 0;
 
-  //Created check function to see if the MetaMask extension is installed
-  const isMetaMaskInstalled = () => {
-    //Have to check the ethereum binding on the window object to see if it's installed
-    const { ethereum } = window;
+	//Created check function to see if the MetaMask extension is installed
+	const isMetaMaskInstalled = () => {
+		//Have to check the ethereum binding on the window object to see if it's installed
+		const { ethereum } = window;
 
-    return Boolean(ethereum && ethereum.isMetaMask);
-  };
+		return Boolean(ethereum && ethereum.isMetaMask);
+	};
 
-  const MetamaskClientCheck = async() => {
-    //Now we check to see if Metmask is installed
-    if (!isMetaMaskInstalled()) {
-      //If it isn't installed we ask the user to click to install it
-      //onboardButton.innerText = 'Click here to install MetaMask!';
-      alert('Please install MetaMask');
-    } else if(!isMetaMaskConnected()) {
-      //If MetaMask is installed we ask the user to connect to their wallet
-      //onboardButton.innerText = 'Connect';
-      onClickConnect();
-    }
+	const MetamaskClientCheck = async () => {
+		//Now we check to see if Metmask is installed
+		if (!isMetaMaskInstalled()) {
+			//If it isn't installed we ask the user to click to install it
+			//onboardButton.innerText = 'Click here to install MetaMask!';
+			alert('Please install MetaMask');
+		} else if (!isMetaMaskConnected()) {
+			//If MetaMask is installed we ask the user to connect to their wallet
+			//onboardButton.innerText = 'Connect';
+			onClickConnect();
+		}
+	};
 
-  };
-
-  //const onboarding = new MetamaskOnboarding({ forwarderOrigin });
-  MetamaskClientCheck();
-  // nftUri();
-}
-
+	//const onboarding = new MetamaskOnboarding({ forwarderOrigin });
+	MetamaskClientCheck();
+	// nftUri();
+};
 
 export const onClickConnect = async () => {
-
-  const { ethereum } = window;
-  if (ethereum && ethereum.isMetaMask) {
-    console.log('Ethereum successfully detected!');
-    // Access the decentralized web!
-  } else {
-    console.log('Please install MetaMask!');
-  }
-/*  if (window.ethereum) {
+	const { ethereum } = window;
+	if (ethereum && ethereum.isMetaMask) {
+		console.log('Ethereum successfully detected!');
+		// Access the decentralized web!
+	} else {
+		console.log('Please install MetaMask!');
+	}
+	/*  if (window.ethereum) {
     try {
       const res = await window.ethereum.request({
         method: "eth_requestAccounts",
@@ -119,23 +116,23 @@ export const onClickConnect = async () => {
     alert("Install MetaMask");
   }*/
 
-  // try {
-  //   const { ethereum } = window;
-  //   if (!isMetaMaskInstalled()){
-  //     alert('Please Install Metamask');
-  //   }
-  //   //Will Start the MetaMask Extension
-  //   let accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-  //   if(accounts[0]!= ''){
-  //     return accounts[0];
-  //     // $("#connect_wallet").html("Connected");
-  //     // $("#connect_wallet").prop( "disabled", true );
-  //   }else{
-  //     alert('Something Wrong. Metamask not connected');
-  //     return  false;
-  //   }
+	// try {
+	//   const { ethereum } = window;
+	//   if (!isMetaMaskInstalled()){
+	//     alert('Please Install Metamask');
+	//   }
+	//   //Will Start the MetaMask Extension
+	//   let accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+	//   if(accounts[0]!= ''){
+	//     return accounts[0];
+	//     // $("#connect_wallet").html("Connected");
+	//     // $("#connect_wallet").prop( "disabled", true );
+	//   }else{
+	//     alert('Something Wrong. Metamask not connected');
+	//     return  false;
+	//   }
 
-  // } catch (error) {
-  //   console.error(error);
-  // }
+	// } catch (error) {
+	//   console.error(error);
+	// }
 };
--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,217 +1,310 @@
-import React, { useEffect, useState, useRef } from "react";
-import Header from "../../components/Others/Header/Header";
-import {Tabs, Modal, useMantineTheme} from "@mantine/core";
-import "./Dashboard.css";
-import Trophy from "../../assets/green_trophy.png";
-import Position from "../../assets/position.png";
-import QR from "../../assets/scan.png";
-import { useNavigate } from "react-router-dom";
-import { Badge } from "@mantine/core";
-import QRCodeReader from "../../components/QRCode/QRScan";
-import { useDispatch, useSelector } from "react-redux";
-import { getLeadingUsers, getMe } from "../../feature/user/userSlice";
-import { API_URL, getToken } from "../../Helper/helper";
-import axios from "axios";
-
+import { Badge, Modal, Tabs, useMantineTheme } from '@mantine/core';
+import axios from 'axios';
+import { useEffect, useRef, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import Trophy from '../../assets/green_trophy.png';
+import Position from '../../assets/position.png';
+import QR from '../../assets/scan.png';
+import Header from '../../components/Others/Header/Header';
+import QRCodeReader from '../../components/QRCode/QRScan';
+import { getLeadingUsers, getMe } from '../../feature/user/userSlice';
+import { API_URL, getToken } from '../../Helper/helper';
+import './Dashboard.css';
 const Dashboard = () => {
-  const [openQR, setOpenQR] = useState(false);
-  const [showLB, setShowLb] = useState(0);
-  const [qrData, setQrData] = useState("");
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-  const lead = useSelector((state) => state?.user?.lead);
-  const qrRef = useRef(null);
-  const [point, setPoint] = useState(0);
-  const [position, setPosition] = useState(0);
-  const [openedP, setOpenedP] = useState(false);
-  const theme = useMantineTheme();
-  useEffect(() => {
-    if (!getToken()) {
-      navigate("/");
-    }
-    dispatch(getLeadingUsers());
-    dispatch(getMe());
-    axios({
-      method: "get",
-      url: `${API_URL}/get/my_position`,
-      headers: {
-        Authorization: `Bearer ${getToken()}`,
-      },
-    }).then((res) => {
-      if (res.status === 200) {
-        setPoint(res?.data?.data?.point);
-        setPosition(res?.data?.data?.position);
-      }
-    });
-  }, []);
+	const [openQR, setOpenQR] = useState(false);
+	const [showLB, setShowLb] = useState(0);
+	const [qrData, setQrData] = useState('');
+	const navigate = useNavigate();
+	const dispatch = useDispatch();
+	const lead = useSelector((state) => state?.user?.lead);
+	const qrRef = useRef(null);
+	const [point, setPoint] = useState(0);
+	const [position, setPosition] = useState(0);
+	const [openedP, setOpenedP] = useState(false);
+	const [exReward, setExReward] = useState([]);
+	const [myReward, setMyReward] = useState('');
 
-  console.log(showLB);
-  const handleOpenQRCode = () => {
-    setQrData("");
-    setOpenQR(true);
-  };
+	const theme = useMantineTheme();
+	useEffect(() => {
+		if (!getToken()) {
+			navigate('/');
+		}
+		dispatch(getLeadingUsers());
+		dispatch(getMe());
+		axios({
+			method: 'get',
+			url: `${API_URL}/get/my_position`,
+			headers: {
+				Authorization: `Bearer ${getToken()}`,
+			},
+		}).then((res) => {
+			if (res.status === 200) {
+				setPoint(res?.data?.data?.point);
+				setPosition(res?.data?.data?.position);
+			}
+		});
+	}, [0]);
 
-  const handleScan = (data) => {
-    console.log(data);
-    if (!!data) {
-      setQrData(data);
-      setOpenQR(false);
+	useEffect(() => {
+		axios({
+			method: 'get',
+			url: `${API_URL}/explorer`,
+			headers: {
+				Authorization: `Bearer ${getToken()}`,
+			},
+		})
+			.then((res) => {
+				if (res.status === 200) {
+					setExReward(res?.data?.data);
+				}
+			})
+			.catch((error) => {
+				// handle error
+				console.log(error);
+			});
+	}, [0]);
 
-      navigate("/quiz", {
-        state: {
-          quizId: data,
-        },
-      });
-      qrRef.current.stopCamera();
-    }
-  };
-  const changeTab = (active) => {
-    setShowLb(active)
-  }
-  return (
-    <div className="dash">
-      <Header />
-      <div
-        className="prog-lead"
-        style={{ maxHeight: "50vh", overflow: "auto" }}
-      >
-        <Tabs color="dark" variant="pills" active={showLB} onTabChange={changeTab}>
-          <Tabs.Tab label="My Progess">
-            <div className="tab-con">
-              <div className="block1 d-flex-column">
-                <div className="d-flex">
-                  <h1 style={{ marginRight: "0.3rem" }}>{point}</h1>
-                  <p>pts</p>
-                </div>
-                <img src={Trophy} alt="trophy" />
-              </div>
-              <div onClick={() => setShowLb(1)} className="block2 d-flex-column">
-                <div className="d-flex">
-                  <h1 style={{ marginRight: "0.3rem" }}>{position}</h1>
-                  <p>position</p>
-                </div>
-                <img src={Position} alt="position" />
-              </div>
-            </div>
-          </Tabs.Tab>
-          <Tabs.Tab label="Leaderboard">
-            { showLB? (
-                <div className="d-flex-column lead">
-                  {Array.isArray(lead) &&
-                      lead.map((user, index) => {
-                        let rankStyle = "";
-                        if (index === 0) {
-                          rankStyle = "first";
-                        }
-                        if (index === 1) {
-                          rankStyle = "second";
-                        }
-                        if (index === 2) {
-                          rankStyle = "third";
-                        }
+	useEffect(() => {
+		// MY reward
+		axios({
+			method: 'get',
+			url: `${API_URL}/position`,
+			headers: {
+				Authorization: `Bearer ${getToken()}`,
+			},
+		})
+			.then((res) => {
+				if (res.status === 200) {
+					setMyReward(res?.data?.data);
+				}
+			})
+			.catch((error) => {
+				// handle error
+				console.log(error);
+			});
+	}, [0]);
+	// console.log(myReward);
+	// console.log(showLB);
+	const handleOpenQRCode = () => {
+		setQrData('');
+		setOpenQR(true);
+	};
 
-                        return (
-                            <div className={`${rankStyle} top-ranks`}>
-                              <div className="d-flex">
-                                <h2>#{index + 1}</h2>
-                                <p style={{ marginLeft: "1rem" }}>@{user.name}</p>
-                              </div>
-                              <Badge
-                                  size="lg"
-                                  style={{
-                                    borderRadius: "5px",
-                                    textTransform: "capitalize",
-                                    color: "white",
-                                    backgroundColor: "#ffb636",
-                                    padding: "0.8rem 0.3rem",
-                                  }}
-                              >
-                                {user.total_point ? user.total_point : 0} pts
-                              </Badge>
-                            </div>
-                        );
-                      })}
-                </div>
-            ): (
-                <div></div>
-            )}
-          </Tabs.Tab>
-        </Tabs>
-        <p
-          className="text-center"
-          style={{ opacity: "0.4", marginTop: "1.2rem", fontSize: "0.8rem" }}
-          onClick={() => setOpenedP(true)}
-        >
-          {" "}
-          <i> How do i earn more points ?</i>
-        </p>
-      </div>
+	const handleScan = (data) => {
+		console.log(data);
+		if (!!data) {
+			setQrData(data);
+			setOpenQR(false);
 
-      {/* Rewards */}
-      <div className="prog-lead" style={{ marginTop: "1rem" }}>
-        <Tabs color="dark" variant="pills">
-          <Tabs.Tab label="My rewards">
-            <div className="tab-con">
-              <div>
-                <h2>Scan more QRs to win exciting rewards!</h2>
-              </div>
-            </div>
-          </Tabs.Tab>
-          <Tabs.Tab label="Explore rewards">
-            <h2>Coming Soon...</h2>
-          </Tabs.Tab>
-        </Tabs>
-      </div>
-      <div className="qr-div">
-        <button onClick={handleOpenQRCode} className="red-btn">
-          <img src={QR} alt="scan" /> SCAN QR
-        </button>
-      </div>
-      {openQR && <QRCodeReader ref={qrRef} handleScan={handleScan} />}
+			navigate('/quiz', {
+				state: {
+					quizId: data,
+				},
+			});
+			qrRef.current.stopCamera();
+		}
+	};
+	const changeTab = (active) => {
+		setShowLb(active);
+	};
 
-      <Modal
-          overlayColor={
-            theme.colorScheme === "dark"
-                ? theme.colors.dark[9]
-                : theme.colors.gray[8]
-          }
-          overlayOpacity={0.55}
-          overlayBlur={3}
-          centered
-          opened={openedP}
-          onClose={() => setOpenedP(false)}
-      >
-        <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              justifyContent: "center",
-              fontSize: "14px",
-              padding: "1rem",
-              marginTop: "-3rem",
-            }}
-        >
-          <h1>Guide</h1>
-          <ul style={{
-            padding: 0,
-            margin: 0,
-            listStyle: 'auto',
-            marginLeft: "15px"
-          }}>
-            <li style={{paddingBottom: ".5rem"}}> Search for Beacons/QR codes in the event area</li>
-            <li style={{paddingBottom: ".5rem"}}> Scan the QRs within the Application using the “Scan QR” button.</li>
-            <li style={{paddingBottom: ".5rem"}}> Answer the questions and earn points</li>
-            <li style={{paddingBottom: ".5rem"}}> As per the leaderboard you will be receiving the NFT rewards once the
-              event comes to an end!
-            </li>
-            <li style={{paddingBottom: ".5rem"}}> Good Luck!</li>
-          </ul>
-        </div>
-      </Modal>
-    </div>
+	return (
+		<div className="dash">
+			<Header />
+			<div
+				className="prog-lead"
+				style={{ maxHeight: '50vh', overflow: 'auto' }}>
+				<Tabs
+					color="dark"
+					variant="pills"
+					active={showLB}
+					onTabChange={changeTab}>
+					<Tabs.Tab label="My Progess">
+						<div className="tab-con">
+							<div className="block1 d-flex-column">
+								<div className="d-flex">
+									<h1 style={{ marginRight: '0.3rem' }}>{point}</h1>
+									<p>pts</p>
+								</div>
+								<img src={Trophy} alt="trophy" />
+							</div>
+							<div
+								onClick={() => setShowLb(1)}
+								className="block2 d-flex-column">
+								<div className="d-flex">
+									<h1 style={{ marginRight: '0.3rem' }}>{position}</h1>
+									<p>position</p>
+								</div>
+								<img src={Position} alt="position" />
+							</div>
+						</div>
+					</Tabs.Tab>
+					<Tabs.Tab label="Leaderboard">
+						{showLB ? (
+							<div className="d-flex-column lead">
+								{Array.isArray(lead) &&
+									lead.map((user, index) => {
+										let rankStyle = '';
+										if (index === 0) {
+											rankStyle = 'first';
+										}
+										if (index === 1) {
+											rankStyle = 'second';
+										}
+										if (index === 2) {
+											rankStyle = 'third';
+										}
 
-  );
+										return (
+											<div className={`${rankStyle} top-ranks`}>
+												<div className="d-flex">
+													<h2>#{index + 1}</h2>
+													<p style={{ marginLeft: '1rem' }}>@{user.name}</p>
+												</div>
+												<Badge
+													size="lg"
+													style={{
+														borderRadius: '5px',
+														textTransform: 'capitalize',
+														color: 'white',
+														backgroundColor: '#ffb636',
+														padding: '0.8rem 0.3rem',
+													}}>
+													{user.total_point ? user.total_point : 0} pts
+												</Badge>
+											</div>
+										);
+									})}
+							</div>
+						) : (
+							<div></div>
+						)}
+					</Tabs.Tab>
+				</Tabs>
+				<p
+					className="text-center"
+					style={{ opacity: '0.4', marginTop: '1.2rem', fontSize: '0.8rem' }}
+					onClick={() => setOpenedP(true)}>
+					{' '}
+					<i> How do i earn more points ?</i>
+				</p>
+			</div>
+
+			{/* Rewards */}
+			<div className="prog-lead" style={{ marginTop: '1rem' }}>
+				<Tabs color="dark" variant="pills">
+					<Tabs.Tab label="My rewards">
+						<div className="rewards_items">
+							{myReward ? (
+								<div className="reward_item">
+									<div className="reward_icon">
+										<img src={`${myReward?.image}`} alt={myReward?.title} />
+									</div>
+									<div className="reward_info">
+										<div className="reward_name">
+											<h3>Reward #{myReward?.id}</h3>
+											<p>{myReward?.title}</p>
+										</div>
+										<a href={myReward?.url} target="_blank">
+											Hello
+										</a>
+									</div>
+								</div>
+							) : (
+								<div>
+									<p style={{ textAlign: 'center', padding: '10px 0' }}>
+										<strong>You Don't Have Any Rewards.</strong>
+									</p>
+								</div>
+							)}
+						</div>
+						<div className="tab-con">
+							<div>
+								<h2>Scan more QRs to win exciting rewards!</h2>
+							</div>
+						</div>
+					</Tabs.Tab>
+					<Tabs.Tab label="Explore rewards">
+						<div className="rewards_items">
+							{exReward.map((reward) => {
+								return (
+									<div className="reward_item">
+										<div className="reward_icon">
+											<img src={`${reward?.image}`} alt={reward?.title} />
+										</div>
+										<div className="reward_info">
+											<div className="reward_name">
+												<h3>Reward #{reward.id}</h3>
+												<p>{reward.title}</p>
+											</div>
+										</div>
+									</div>
+								);
+							})}
+						</div>
+					</Tabs.Tab>
+				</Tabs>
+			</div>
+			<div className="qr-div">
+				<button onClick={handleOpenQRCode} className="red-btn">
+					<img src={QR} alt="scan" /> SCAN QR
+				</button>
+			</div>
+			{openQR && <QRCodeReader ref={qrRef} handleScan={handleScan} />}
+
+			<Modal
+				overlayColor={
+					theme.colorScheme === 'dark'
+						? theme.colors.dark[9]
+						: theme.colors.gray[8]
+				}
+				overlayOpacity={0.55}
+				overlayBlur={3}
+				centered
+				opened={openedP}
+				onClose={() => setOpenedP(false)}>
+				<div
+					style={{
+						display: 'flex',
+						flexDirection: 'column',
+						alignItems: 'center',
+						justifyContent: 'center',
+						fontSize: '14px',
+						padding: '1rem',
+						marginTop: '-3rem',
+					}}>
+					<h1>Guide</h1>
+					<ul
+						style={{
+							padding: 0,
+							margin: 0,
+							listStyle: 'auto',
+							marginLeft: '15px',
+						}}>
+						<li style={{ paddingBottom: '.5rem' }}>
+							{' '}
+							Search for Beacons/QR codes in the event area
+						</li>
+						<li style={{ paddingBottom: '.5rem' }}>
+							{' '}
+							Scan the QRs within the Application using the “Scan QR” button.
+						</li>
+						<li style={{ paddingBottom: '.5rem' }}>
+							{' '}
+							Answer the questions and earn points
+						</li>
+						<li style={{ paddingBottom: '.5rem' }}>
+							{' '}
+							As per the leaderboard you will be receiving the NFT rewards once
+							the event comes to an end!
+						</li>
+						<li style={{ paddingBottom: '.5rem' }}> Good Luck!</li>
+					</ul>
+				</div>
+			</Modal>
+		</div>
+	);
 };
 
 export default Dashboard;
